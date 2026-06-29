5238367d97db0fa3150474f17f73a6b5b19a6d49
nokupr1 <prodbydominus@gmail.com>
2026-06-29 19:47:55 +0700

    refactor: Refactor project structure

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 from .config import settings
 
 engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
@@ -8,12 +9,14 @@ engine = create_engine(settings.database_url, connect_args={"check_same_thread":
 SessionLocal = sessionmaker(autoflush=False, expire_on_commit=False, bind=engine)
 Base = declarative_base()
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
 
 def init_db():
     Base.metadata.create_all(bind=engine)
